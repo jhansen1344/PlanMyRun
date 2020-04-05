1a02f258c0a41ff8e12ab22416ae34c95a427290
Jeremy Hansen <jhansen1344@gmail.com>
2020-04-05 10:27:52 -0400

    display on forecast

--- PlanMyRun.Models/ForecastModels/ForecastDayModel.cs
+++ PlanMyRun.Models/ForecastModels/ForecastDayModel.cs
@@ -11,14 +11,21 @@ namespace PlanMyRun.Models.ForecastModels
     {
         public string Date { get; set; }
         [Display(Name = "Sunrise")]
+        [DisplayFormat(DataFormatString = "{0:hh\\:mm}")]
         public TimeSpan Sunrise_Time { get; set; }
         [Display(Name = "Sunset")]
+        [DisplayFormat(DataFormatString = "{0:hh\\:mm}")]
         public TimeSpan Sunset_Time { get; set; }
         [Display(Name = "High Temp")]
+        [DisplayFormat(DataFormatString = "{0:#.}")]
         public decimal Temp_Max_F { get; set; }
+ 
         [Display(Name = "Low Temp")]
+        [DisplayFormat(DataFormatString = "{0:#.}")]
         public decimal Temp_Min_F { get; set; }
-        [Display(Name = "Precip Chances")]
+
+        [Display(Name = "Precip.")]
+        [DisplayFormat(DataFormatString = "{0:#.}")]
         public decimal Prob_Precip_Pct { get; set; }
         public IEnumerable<ForecastHourlyModel> TimeFrames { get; set; }
     }
